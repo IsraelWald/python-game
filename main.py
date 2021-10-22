e5632a6ffad572ae194a9316d2629afdd0e7cf80
Israel Waldner <isaelwaldner@g.horrycountyschools.net>
2021-10-22 08:59:58 -0400

    Added arrow shooting

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ screen = pygame.display.set_mode(screen_size)
 keys = [False, False, False, False]
 player_pos = [100, 100]
 
+# shooting things
+acc = [0, 0]
+arrows = []
+
 
 def load(filename):
 	return pygame.image.load(f"resources/images/{filename}")
@@ -19,6 +23,7 @@ def load(filename):
 player = load("dude.png")
 grass = load("grass.png")
 castle = load("castle.png")
+arrow = load("arrow.png")
 
 while True:
 	screen.fill(0)
@@ -43,6 +48,22 @@ while True:
 		player_pos[0] - player_angle.get_rect().width / 2,
 		player_pos[1] - player_angle.get_rect().height / 2,
 	)
+
+		# 6.2 - Draw arrows
+	for bullet in arrows:
+		index=0
+		velx=math.cos(bullet[0])*10
+		vely=math.sin(bullet[0])*10
+		bullet[1]+=velx
+		bullet[2]+=vely
+		if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
+			arrows.pop(index)
+		index+=1
+		for projectile in arrows:
+			arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
+			screen.blit(arrow1, (projectile[1], projectile[2]))
+
+
 	screen.blit(player_angle, playerpos1)
 	pygame.display.flip()
 
@@ -68,6 +89,19 @@ while True:
 				keys[2] = False
 			elif event.key == pygame.K_a:
 				keys[3] = False
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			position = pygame.mouse.get_pos()
+			acc[1] += 1
+			arrows.append(
+				[
+					math.atan2(
+						position[1] - (playerpos1[1] + 32),
+						position[0] - (playerpos1[0] + 26),
+					),
+					playerpos1[0] + 32,
+					playerpos1[1] + 32,
+				]
+			)
 	if keys[0]:
 		player_pos[1] -= 5
 	if keys[1]:
